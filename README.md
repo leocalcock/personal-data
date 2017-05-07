46816105651b578e5a8501e29f7ae8efc921c825
leoa9001 <leoa9001@gmail.com>
2017-05-07 19:39:07 -0400

    wrote setTasks()
    function which gets data on which tasks to be tracked by whichever
    variables.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # personal-data
+At the moment this is just a collection of scripts for collecting and organizing personal data for analysis. 
+
+
+##Codebook:
+
+###Habitica 
+The habiticaFuncs.R contains various functions collecting data from your habitica account. Use `setup()` to create the connection between your
+account and the system (e.g. `setup(apiUser,apiKey)`). You can set all of your data tracking in habitica by editing the "notes" section of any
+given task. Add on the last line of some task's notes a "#variablename" and after doing this for all tasks you wish to track data for, run `getTasks()`. Every day, the scripts will download and organize your data in a neat spreadsheet with columns corresponding to the variablenames given (next thing to set up)
--- data collection/habiticaFuncs.r
+++ data collection/habiticaFuncs.r
@@ -3,7 +3,7 @@ library(httr)
 library(jsonlite)
 
 
-# path is path to the data folder.
+# path is path to the data folder. currently does not install R packages
 setup <- function(user, pw, path = "../data"){
       if(!file.exists(paste0(path,"/habitica"))){
             p2 <- paste0(path,"/habitica")
@@ -15,7 +15,7 @@ setup <- function(user, pw, path = "../data"){
             close(fc)
             
             fc2 <- file(paste0(p2,"/taskIDs.json"))
-            writeLines(c("{\"id\":[]}"), fc2)
+            writeLines(c("{\"ids\":[]}, \"variables\":[]}"), fc2)
             close(fc2)
       }else{
             print("habitica already set up.")
@@ -59,7 +59,59 @@ getTasks <- function(user = NULL, pw = NULL){
       gr <- GET("https://habitica.com/api/v3/tasks/user",
                 add_headers(.headers = c("x-api-user" = user,"x-api-key" = pw)))
       if(gr$status_code == 200){
-            ret = content(gr,"text")
+            ret = content(gr,"text","fromJSON")
       }
       ret
 }
+
+#id extraction to inform what to keep track of! ! !
+#for now, a task can only be linked to one variable (I'll almost certainly change this.)
+setTasks <- function(uspw = NULL, overwrite = T, path = "../data/habitica"){
+      gt <- NULL
+      if(is.null(uspw)){
+            gt <- getTasks()
+      }else {
+            gt <- getTasks(uspw[1],uspw[2])
+      }
+      
+      data <- fromJSON(gt)$data
+      notes <- data$notes
+      ids <- data$id
+      #finds which vars to track based on note
+      track <- function(note){
+            if(note==""){
+                  c(F, NULL)
+            }else{
+                  splt <- strsplit(note,"\n")[[1]]
+                  line <- splt[length(splt)]
+                  c(substr(line,1,1)=="#", substr(line,2,nchar(line)))
+            }    
+      }
+      #go through notes to find ids, and etc. to append
+      idLs <- vector("character",0)
+      varLs <- vector("character",0)
+      vars <- list()
+      #looping through entries
+      for(i in 1:length(notes)){
+            t <- track(notes[i])
+            if(t[1]){
+                  #print(paste("SUCCESSSSS",ids[i]))
+                  idLs <- c(idLs,as.character(ids[i]))
+                  v <- t[2]
+                  if(v %in% varLs){
+                        print("WHYYYY")
+                        index <- which(v==varLs)
+                        vars[[index]]$ids <- c(vars[[index]]$ids,ids[i])
+                  }else{
+                        varLs <- c(varLs,v)
+                        vars[[length(varLs)]] <- list(varname = as.character(v), ids = c(ids[i]))
+                  }
+            }
+      }
+      js <- as.character(toJSON(list(ids = idLs,variables = vars), pretty = T))
+      fc <- file(paste0(path,"/taskIDs.json"))
+      writeLines(js,fc)
+      close(fc)
+}
+
+
